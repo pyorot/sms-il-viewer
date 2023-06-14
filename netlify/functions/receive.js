5c50e8b7cd056edbe94513b0b2e8010e81ef6c1b
Pyorot <fmasic@hotmail.co.uk>
2023-06-14 21:10:44 +0100

    .

--- netlify/functions/receive.js
+++ netlify/functions/receive.js
@@ -1,10 +1,12 @@
 
 exports.handler = async (event, context) => {
   console.log(event)
-  if(event.httpMethod === 'POST' && event.path === '/upload') {
+  if(event.httpMethod === 'POST') {
     console.log(event.body)
     fs.writeFileSync("data.js", `var data = JSON.parse(\n\`${JSON.stringify(event.body)}\`\n)`)
     // (err) => { if (err) {console.log(err)} }
     return { statusCode: 201, body: 'Accepted' }
+  } else {
+    return { statusCode: 400, body: 'Unknown' }
   }
 }
