ae0f858b427c4c0e615186239eea676e346a4cf8
Pyorot <fmasic@hotmail.co.uk>
2023-06-14 20:49:48 +0100

    netlify function receive.js

---
+++ netlify/functions/receive.js
@@ -0,0 +1,10 @@
+
+exports.handler = function(event, context, callback) {
+    if(event.httpMethod === 'POST' && event.path === '/upload') {
+        console.log(event.body)
+        callback(null, { statusCode: 201, message: 'Accepted' })
+        fs.writeFile("data.js", `var data = JSON.parse(\n\`${JSON.stringify(event.body)}\`\n)`,
+          (err) => { if (err) {console.log(err)} }
+        )
+    }
+}
