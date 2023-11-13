a0099eb6a6d643b6f61d064190edfadc343dfcd1
Pyorot <fmasic@hotmail.co.uk>
2023-11-13 14:32:34 +0000

    fix: load screen + tooltip formatting

--- js/table.js
+++ js/table.js
@@ -10,6 +10,7 @@ function tableFooterHTML(tableWidth) {
   </td></tr>`
 }
 function tooltipHTML(note) {
+  note = note.replace(/\n/g, "<br>").replace(/(https?\:\/\/\S+)/g, `<a href="$1">$1</a>`) // render newlines and hyperlinks
   return `<div class="tooltip">📝<div class="tooltipbox"><div class="tooltiptext">${note}</div></div></div>`
 }
 
